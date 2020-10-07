e84095e6414eaf2fd7e695c80c2b7826baab8f3d
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2020-10-07 01:59:45 -0400

    Add timer interval

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-var timer=documtent.getElementById("")
+var timer = document.getElementById("timer");
 
-function timer () {
-  
+var secondsLeft = 75;
+timer.textContent = "Time: " + secondsLeft;
+
+function timeLeft () {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = "Time: " + secondsLeft;
+    
+    if(secondsLeft === 0){
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
 }
+
+timeLeft();
